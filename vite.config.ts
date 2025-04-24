b7122685d3fcc4334d961f7db864135f4d215129
Paul Kang <kbu715@gmail.com>
2025-04-24 19:25:40 +0900

    배포 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+
+const mode = process.env.NODE_ENV || "development";
+const env = loadEnv(mode, process.cwd(), "");
 
 export default mergeConfig(
   defineConfig({
+    base: env.VITE_BASE_PATH,
     plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.refactoring.html"),
+        },
+      },
+    },
   }),
   defineTestConfig({
     test: {
       globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
     },
   })
-)
+);
